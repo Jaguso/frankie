c15bd3cb4d90f314444c22729aab9b52e746496c
jpazm <jaime@pazmental.mx>
2019-09-05 10:10:59 -0500

    Notifications works now!

--- App.js
+++ App.js
@@ -51,8 +51,15 @@ class App extends Component<Props> {
 
   async componentDidMount() {
     this.checkPermission();
+    this.createNotificationListeners();
   }
   
+    //Remove listeners allocated in createNotificationListeners()
+  componentWillUnmount() {
+    this.notificationListener();
+    this.notificationOpenedListener();
+  }
+
     //1
   async checkPermission() {
     const enabled = await firebase.messaging().hasPermission();
@@ -67,6 +74,7 @@ class App extends Component<Props> {
   async getToken() {
     let fcmToken = await AsyncStorage.getItem('fcmToken');
     console.warn('fcmtoken >>', fcmToken)
+    console.log('fcmtoken >>', fcmToken)
     if (!fcmToken) {
         fcmToken = await firebase.messaging().getToken();
         if (fcmToken) {
@@ -87,6 +95,52 @@ class App extends Component<Props> {
         console.log('permission rejected');
     }
   }
+
+
+  async createNotificationListeners() {
+    /*
+    * Triggered when a particular notification has been received in foreground
+    * */
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+        const { title, body } = notification;
+        this.showAlert(title, body);
+    });
+  
+    /*
+    * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+    * */
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+    });
+  
+    /*
+    * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+    * */
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+    }
+    /*
+    * Triggered for data only payload in foreground
+    * */
+    this.messageListener = firebase.messaging().onMessage((message) => {
+      //process data message
+      console.log(JSON.stringify(message));
+    });
+  }
+
+  showAlert(title, body) {
+    Alert.alert(
+      title, body,
+      [
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false },
+    );
+  }
+  
   
 
   render() {
